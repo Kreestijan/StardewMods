f56e58afe1a58b2c13f27ecb4196e265db81593f
Cristian Pirvu <iliecristianpirvu@gmail.com>
2026-05-06 22:39:34 +0300

    Clean temp files and update nudge defaults

--- PassableFarmAnimals/ModConfig.cs
+++ PassableFarmAnimals/ModConfig.cs
@@ -4,7 +4,7 @@ internal sealed class ModConfig
 {
     public bool EnableMod { get; set; } = true;
     public bool EnableNudge { get; set; } = true;
-    public int NudgeStrengthPixels { get; set; } = 24;
-    public int NudgeDurationMs { get; set; } = 180;
-    public int NudgeCooldownMs { get; set; } = 300;
+    public int NudgeStrengthPixels { get; set; } = 8;
+    public int NudgeDurationMs { get; set; } = 1000;
+    public int NudgeCooldownMs { get; set; } = 2000;
 }
